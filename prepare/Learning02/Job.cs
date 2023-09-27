a1f58c413159cc7ffad48dc8b252edd7fa2a7e21
mr-dolin <u626421740@byui.edu>
2023-09-26 19:00:31 -0600

    w03 Prepare: Learning Activity

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,15 @@
+public class Job
+{
+    public string _company;
+    public string _jobTitle;
+    public int _startYear;
+    public int _endYear;
+
+    public void DisplayJob()
+    {
+        string display = $"{_company} ({_jobTitle}) {_startYear} - {_endYear}";
+        // return _company + _jobTitle + _startYear + _endYear;
+        Console.WriteLine(display);
+    }
+    
+}
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Hoopla Software";
+        job1._jobTitle = "Software Developer";
+        job1._startYear = 2015;
+        job1._endYear = 2020;       
+
+        Job job2 = new Job();
+        job2._company = "Barnacles Development";
+        job2._jobTitle = "Lead Software Engineer";
+        job2._startYear = 2020;
+        job2._endYear = 2022;
+
+        Resume resume1 = new Resume();
+        resume1._name = "Jared Dolinar";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.DisplayResume();
     }
 }
